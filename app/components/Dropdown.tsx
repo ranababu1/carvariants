c2994da904ff979f6459a1a8a8c93b3d5d2c371a
I m r a n <imrn.dev@gmail.com>
2024-08-23 02:36:50 +0530

    dropdown updated

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -1,18 +1,32 @@
-// app/components/Dropdown.tsx
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import React from 'react';
+import { Select, MenuItem, FormControl, InputLabel, SelectChangeEvent } from '@mui/material';
 
-export default function Dropdown({ options, onSelect, label }: { options: string[], onSelect: (option: string) => void, label: string }) {
+interface DropdownProps {
+  label: string;
+  options: string[];
+  onSelect: (value: string) => void;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ label, options, onSelect }) => {
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    onSelect(event.target.value);
+  };
+
   return (
     <FormControl fullWidth sx={{ marginY: 1 }}>
       <InputLabel>{label}</InputLabel>
-      <Select onChange={(e) => onSelect(e.target.value)} label={label}>
+      <Select onChange={handleChange} label={label} defaultValue="">
         <MenuItem value="">
           <em>Select {label}</em>
         </MenuItem>
-        {options.map((option, index) => (
-          <MenuItem key={index} value={option}>{option}</MenuItem>
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
         ))}
       </Select>
     </FormControl>
   );
-}
+};
+
+export default Dropdown;
